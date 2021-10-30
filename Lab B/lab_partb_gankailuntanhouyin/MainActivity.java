101cf26149b631da97aa267e474880f3fe831880
HYTan0909 <87137966+HYTan0909@users.noreply.github.com>
2021-10-30 19:45:15 +0800

    Update MainActivity.java

--- Lab B/lab_partb_gankailuntanhouyin/MainActivity.java
+++ Lab B/lab_partb_gankailuntanhouyin/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.lab_partb_gankailuntanhouyin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,5 +37,32 @@ public class MainActivity extends AppCompatActivity {
         //set take away button active
         m_RbTakeAway.setChecked(true);
 
+    }
+
+    //call menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.food_ordering_menu,menu);
+        return true;
+    }
+
+    //toast message if selected menu item
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_shopping:
+                Toast.makeText(this,"Shopping option selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_print:
+                Toast.makeText(this,"Print option selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_userProfile:
+                Toast.makeText(this,"User Profile option selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_FAQ:
+                Toast.makeText(this,"FAQ option selected",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
