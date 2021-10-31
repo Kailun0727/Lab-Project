4a14b9038210db05e81257fa57de17de3bec208f
Kailun0727 <83753090+Kailun0727@users.noreply.github.com>
2021-10-31 15:01:49 +0800

    Create FAQ.java

---
+++ Lab B/lab_partb_gankailuntanhouyin/FAQ.java
@@ -0,0 +1,43 @@
+package com.example.lab_partb_gankailuntanhouyin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.LinkedList;
+
+public class FAQ extends AppCompatActivity {
+    private final LinkedList<String> mFAQ = new LinkedList<>();
+    private final LinkedList<String> mFAQAnswer = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private FAQListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_a_q);
+        mRecyclerView = findViewById(R.id.rv_FAQ);
+
+        mFAQ.addLast("How to make an order ?");
+        mFAQ.addLast("Can I cancel my order ?");
+        mFAQ.addLast("How to view my order status?");
+        mFAQ.addLast("How long do I need to wait for my order ?");
+        mFAQ.addLast("Any hidden cost for my order ?");
+
+
+        mFAQAnswer.addLast("Select your order type, main course option, snack option to place your order");
+        mFAQAnswer.addLast("Yes, you can cancel your order");
+        mFAQAnswer.addLast("You can view order status in user profile ");
+        mFAQAnswer.addLast("30 - 60 minutes");
+        mFAQAnswer.addLast("No, there is no hidden cost for your order");
+
+        //Create adapter object
+        mAdapter = new FAQListAdapter(this,mFAQ,mFAQ,mFAQAnswer);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
